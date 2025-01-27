021b0ab7c14dca2c5577177a5d13cc987eb9f284
codeParyul <paryul.singhai@sematext.com>
2025-01-27 22:11:39 +0530

    changes docker file

--- python-flask-docker-app-main/app.py
+++ python-flask-docker-app-main/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask Application - Ashok IT"
+    return "Welcome to Python Flask Application - vivek singh bais"
 
 
 if __name__ == "__main__":
